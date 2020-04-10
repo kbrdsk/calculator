900e39be51bba739398e6d5f7d13a1275c82f222
Kabirdas Henry <kbrdsk@gmail.com>
2020-04-10 10:14:21 -0700

    refactoring

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -13,73 +13,68 @@ function appendButtonValue(button){
 }
 
 function parseExpression(expression){
-	//let i = expression.lastIndexOf(')');
 	if(expression.includes('(')){
 		return parseParen(expression);
-		//return parseParen(expression.slice(0, i), expression.slice(i+1));
 	}
 	i = expression.lastIndexOf('+');
 	if(i > -1){
-		return parseAddition(expression.slice(0, i), expression.slice(i+1));
+		return parseAddition(...splitExpression(expression, i));
 	}
 	i = expression.lastIndexOf('-');
 	if(i > -1){
-		return parseSubtraction(expression.slice(0, i), expression.slice(i+1));
+		return parseSubtraction(...splitExpression(expression, i));
 	}
 	i = expression.lastIndexOf('*');
 	if(i > -1){
-		return parseMultiplication(expression.slice(0, i), expression.slice(i+1));
+		return parseMultiplication(...splitExpression(expression, i));
 	}
 	i = expression.lastIndexOf('/');
 	if(i > -1){
-		return parseDivision(expression.slice(0, i), expression.slice(i+1));
+		return parseDivision(...splitExpression(expression, i));
 	}
 	i = expression.lastIndexOf('^');
 	if(i > -1){
-		return parseExponent(expression.slice(0, i), expression.slice(i+1));
+		return parseExponent(...splitExpression(expression, i));
 	}
 	return parseNumber(expression);
 
 }
 
+function splitExpression(expression, index){
+	return [expression.slice(0, index), expression.slice(index + 1)];
+}
+
 function parseParen(expression){
-	let i = expression.lastIndexOf('('),
-		j = expression.indexOf(')', i);
+	let l = expression.lastIndexOf('('),
+		r = expression.indexOf(')', l);
+	let innerExpression = parseExpression(expression.slice(l + 1, r)).toString();
 	let partialParse = 
-			expression.slice(0, i) +
-			parseExpression(expression.slice(i + 1, j)).toString() +
-			expression.slice(j + 1);
+			expression.slice(0, l) +
+			innerExpression +
+			expression.slice(r + 1);
 	return parseExpression(partialParse);
-	/*let i = str1.lastIndexOf(')');
-	let j = str1.lastIndexOf('(');
-	if(i > j){
-		let partialParse = parseParen(str1.slice(0, i), str1.slice(i+1) + ')').toString() + str2;
-		return parseExpression(partialParse);
-	}
-	let partialParse = str1.slice(0, j) + parseExpression(str1.slice(j + 1)).toString() + str2;
-	return parseExpression(partialParse);*/
 }
 
-function parseAddition(str1, str2){
-	return parseExpression(str1) + parseExpression(str2);
+function parseAddition(exp1, exp2){
+	return parseExpression(exp1) + parseExpression(exp2);
 }
 
-function parseSubtraction(str1, str2){
-	return parseExpression(str1) - parseExpression(str2);
+function parseSubtraction(exp1, exp2){
+	return parseExpression(exp1) - parseExpression(exp2);
 }
 
-function parseMultiplication(str1, str2){
-	return parseExpression(str1) * parseExpression(str2);
+function parseMultiplication(exp1, exp2){
+	return parseExpression(exp1) * parseExpression(exp2);
 }
 
-function parseDivision(str1, str2){
-	return parseExpression(str1) / parseExpression(str2);
+function parseDivision(exp1, exp2){
+	return parseExpression(exp1) / parseExpression(exp2);
 }
 
-function parseExponent(str1, str2){
-	return parseExpression(str1) ** parseExpression(str2);
+function parseExponent(exp1, exp2){
+	return parseExpression(exp1) ** parseExpression(exp2);
 }
 
-function parseNumber(str){
-	return +str;
+function parseNumber(exp){
+	return +exp;
 }
